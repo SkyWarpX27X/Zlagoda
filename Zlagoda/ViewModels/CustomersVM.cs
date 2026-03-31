fa361307cc4c02690b4e2655b768d06f2ae60be4
SkyWarpX27X <mykhajloost@gmail.com>
2026-03-31 20:55:23 +0300

    Customers page, VM and test data

---
+++ Zlagoda/ViewModels/CustomersVM.cs
@@ -0,0 +1,24 @@
+﻿using DTOModels;
+using Zlagoda.Test;
+
+namespace Zlagoda.ViewModels;
+
+public class CustomersVM
+{
+    //TODO switch to real customer service
+    //private readonly ICustomerService _customerService;
+    
+    public IEnumerable<CustomerDto> Customers;
+
+    public CustomersVM()
+    {
+        //_customerService = customerService;
+        Customers = new List<CustomerDto>();
+    }
+
+    public void LoadCustomers()
+    {
+        //Customers = _customerService.GetCustomers();
+        Customers = FakeCustomers.GetCustomers();
+    }
+}
