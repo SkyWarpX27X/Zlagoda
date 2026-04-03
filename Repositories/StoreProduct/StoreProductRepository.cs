0c4ab776872f96eab5b83753daa7612caa8c6791
NazariyRomanyuk <naz.romanyuk@gmail.com>
2026-04-03 23:43:06 +0300

    Potentially finish StoreProductRepository.cs? (Might need to add more queries but idk)

--- Repositories/StoreProduct/StoreProductRepository.cs
+++ Repositories/StoreProduct/StoreProductRepository.cs
@@ -19,7 +19,7 @@ public class StoreProductRepository
         reader.GetBoolean(reader.GetOrdinal("promotional_product"))
     );
 
-    public StoreProductDBModel? GetProduct(string upc)
+    public StoreProductDBModel? GetStoreProduct(string upc)
     {
         using var command = _connection.CreateCommand();
         command.CommandText = "SELECT * FROM Product WHERE UPC = @upc";
@@ -28,17 +28,55 @@ public class StoreProductRepository
         return reader.Read() ? MapStoreProduct(reader) : null;
     }
     
-    // Problem: need to get all information (which probably includes name too) so I need to join and create a new entity type?
-    // Also need sorting by name (which also requires joining) and an optional ternary logic parameter (null/promotional/not).
-    public IEnumerable<StoreProductDBModel> GetProducts(bool sortByName = true, bool sortByQuantity = true)
+    public IEnumerable<StoreProductDBModel> GetStoreProducts(bool sortByName = true, bool sortByQuantity = false)
     {
         using var command = _connection.CreateCommand();
+        // Since we join, it technically returns all attributes. Is that necessarily bad? I'm not sure, since I think
+        // SELECT basically just does a projection on the final result anyway, and we just ignore that by mapping it to
+        // the DB model.
         var query = "SELECT * FROM Store_Product";
-
-        if (sortByName) query += " ORDER BY product_name";
+        if (sortByName || sortByQuantity) 
+            query += $"""
+                     JOIN Product ON Store_Product.id_product = Product.id_product
+                     ORDER BY {(sortByName ? "product_name" : "products_number")}
+                     """;
         command.CommandText = query;
         using var reader = command.ExecuteReader();
         while (reader.Read())
             yield return MapStoreProduct(reader);
     }
+
+    public IEnumerable<StoreProductDBModel> GetStoreProductsPromotional(bool sortByName = true, bool sortByQuantity = false)
+    {
+        using var command = _connection.CreateCommand();
+        var query = "SELECT * FROM Store_Product";
+        if (sortByName || sortByQuantity) 
+            query += $"""
+                      JOIN Product ON Store_Product.id_product = Product.id_product
+                      WHERE promotional_product IS TRUE
+                      ORDER BY {(sortByName ? "product_name" : "products_number")}
+                      """;
+        command.CommandText = query;
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+            yield return MapStoreProduct(reader);
+    }
+    
+    public IEnumerable<StoreProductDBModel> GetStoreProductsNonPromotional(bool sortByName = true, bool sortByQuantity = false)
+    {
+        using var command = _connection.CreateCommand();
+        var query = "SELECT * FROM Store_Product";
+        if (sortByName || sortByQuantity) 
+            query += $"""
+                      JOIN Product ON Store_Product.id_product = Product.id_product
+                      WHERE promotional_product IS FALSE
+                      ORDER BY {(sortByName ? "product_name" : "products_number")}
+                      """;
+        command.CommandText = query;
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+            yield return MapStoreProduct(reader);
+    }
+    
+    // IDK if I should add Manager query 21 here or not it feels very general help
 }
