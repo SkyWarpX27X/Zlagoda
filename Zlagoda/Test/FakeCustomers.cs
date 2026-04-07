5ed2f770afd2f36d71f3f0e6997a22341438ac1e
SkyWarpX27X <mykhajloost@gmail.com>
2026-04-07 15:31:58 +0300

    Customers CRUD frontend

--- Zlagoda/Test/FakeCustomers.cs
+++ Zlagoda/Test/FakeCustomers.cs
@@ -4,18 +4,18 @@ namespace Zlagoda.Test;
 
 static class FakeCustomers
 {
-    static IEnumerable<CustomerDto> _customers;
+    static IEnumerable<CustomerDTO> _customers;
     
     static FakeCustomers()
     {
-        _customers = new List<CustomerDto>()
+        _customers = new List<CustomerDTO>()
         {
-            new CustomerDto("1", "Kernes Hennadiy Adolfovych", "+380991112233", "Kharkiv Knopochna street 10a 12345", 1),
-            new CustomerDto("2", "Таємний Африкан Свиридович", "101", "MISSINGNO MISSINGNO 00000", 5)
+            new CustomerDTO("1", "Kernes Hennadiy Adolfovych", "+380991112233", "Kharkiv, Knopochna street 10a, 12345", 1),
+            new CustomerDTO("2", "Таємний Африкан Свиридович", "101", "MISSINGNO, MISSINGNO, 00000", 5)
         };
     }
 
-    public static IEnumerable<CustomerDto> GetCustomers()
+    public static IEnumerable<CustomerDTO> GetCustomers()
     {
         return _customers;
     }
--- Zlagoda/ViewModels/CustomersVM.cs
+++ Zlagoda/ViewModels/CustomersVM.cs
@@ -8,17 +8,53 @@ public class CustomersVM
     //TODO switch to real customer service
     //private readonly ICustomerService _customerService;
     
-    public IEnumerable<CustomerDto> Customers;
-
+    public IEnumerable<CustomerDTO> Customers;
+    public bool IsCreating;
+    public CustomerModifyDTO? NewCustomer;
+    
     public CustomersVM()
     {
         //_customerService = customerService;
-        Customers = new List<CustomerDto>();
+        Customers = new List<CustomerDTO>();
     }
 
     public void LoadCustomers()
     {
         //Customers = _customerService.GetCustomers();
         Customers = FakeCustomers.GetCustomers();
+    }
+    
+    public void ShowCreateNew()
+    {
+        NewCustomer = new CustomerModifyDTO();
+        IsCreating = true;
+    }
+    
+    public void CancelCreate()
+    {
+        IsCreating = false;
+        NewCustomer = null;
+    }
+    
+    public void SaveNewCustomer(CustomerModifyDTO customer)
+    {
+        var foo = 1;
+        //_customerService.AddCustomer(customer);
+        IsCreating = false;
+        LoadCustomers();
+    }
+
+    public void EditCustomer(CustomerModifyDTO customer)
+    {
+        var foo = 1;
+        //_customerService.UpdateCustomer(customer);
+        LoadCustomers();
+    }
+
+    public void DeleteCustomer(string cardId)
+    {
+        var foo = 1;
+        //_customerService.DeleteCustomer(customer);
+        LoadCustomers();
     }
 }
