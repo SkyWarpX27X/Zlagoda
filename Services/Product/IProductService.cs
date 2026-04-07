6d3a64d9a08efa2f2c1bf384210077b7b89009f9
SkyWarpX27X <mykhajloost@gmail.com>
2026-04-07 16:10:59 +0300

    Product service interface

---
+++ Services/Product/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using DTOModels;
+
+namespace Services.Product;
+
+public interface IProductService
+{
+    IEnumerable<ProductDTO> GetProducts();
+    ProductDTO GetProduct(int id);
+    void AddProduct(ProductDTO customer);
+    void UpdateProduct(ProductDTO customer);
+    void DeleteProduct(int id);
+}
