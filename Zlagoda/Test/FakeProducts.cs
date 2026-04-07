dac1c354ce6d4aa25f597ce21b23e4a63b11cf6e
SkyWarpX27X <mykhajloost@gmail.com>
2026-04-07 16:10:21 +0300

    Naming fix

--- Zlagoda/Test/FakeProducts.cs
+++ Zlagoda/Test/FakeProducts.cs
@@ -4,19 +4,19 @@ namespace Zlagoda.Test;
 
 static class FakeProducts
 {
-    static IEnumerable<ProductDto> _products;
+    static IEnumerable<ProductDTO> _products;
 
     static FakeProducts()
     {
-        _products = new List<ProductDto>()
+        _products = new List<ProductDTO>()
         {
-            new ProductDto(1, "Кілограм щура", "Товари подвійного призначення", "Екологи вішаються", "ПХЗ"),
-            new ProductDto(2, "Газета 'Кишинівський Підмічатель'", "Розваги",
+            new ProductDTO(1, "Кілограм щура", "Товари подвійного призначення", "Екологи вішаються", "ПХЗ"),
+            new ProductDTO(2, "Газета 'Кишинівський Підмічатель'", "Розваги",
                 "Премія 'Газета року 2077'", "Rousseau&Reese")
         };
     }
 
-    public static IEnumerable<ProductDto> GetProducts()
+    public static IEnumerable<ProductDTO> GetProducts()
     {
         return _products;
     }
