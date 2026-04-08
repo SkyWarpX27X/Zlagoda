6b712f7c6013032ee96d93414b798f384daffb46
SkyWarpX27X <mykhajloost@gmail.com>
2026-04-08 11:52:55 +0300

    Rewrite categories page and add CRUD frontend

---
+++ Zlagoda/Test/FakeCategories.cs
@@ -0,0 +1,22 @@
+﻿using DTOModels;
+
+namespace Zlagoda.Test;
+
+static class FakeCategories
+{
+    static IEnumerable<CategoryDto> _categories;
+
+    static FakeCategories()
+    {
+        _categories = new List<CategoryDto>()
+        {
+            new CategoryDto(1, "Картон"),
+            new CategoryDto(2, "Товари подвійного призначення")
+        };
+    }
+
+    public static IEnumerable<CategoryDto> GetCategories()
+    {
+        return _categories;
+    }
+}
--- Zlagoda/ViewModels/CategoriesVM.cs
+++ Zlagoda/ViewModels/CategoriesVM.cs
@@ -2,22 +2,60 @@
 using Services;
 using System.Threading.Tasks;
 using Services.Category;
+using Zlagoda.Test;
 
 namespace Zlagoda.ViewModels;
 
 public class CategoriesVM
 {
-    private readonly ICategoryService _categoryService;
+    //TODO replace with real service
+    //private readonly ICategoryService _categoryService;
     public IEnumerable<CategoryDto> Categories { get; set; }
     
-    public CategoriesVM(ICategoryService categoryService)
+    public bool IsCreating { get; private set; }
+    public CategoryDto? NewCategory { get; private set; }
+    
+    public CategoriesVM()
     {
-        _categoryService = categoryService;
+        //_categoryService = categoryService;
         Categories = new List<CategoryDto>();
     }
 
     public void LoadCategories()
     {
-        Categories = _categoryService.GetCategories();
+        //Categories = _categoryService.GetCategories();
+        Categories = FakeCategories.GetCategories();
+    }
+
+    public void ShowCreateNew()
+    {
+        NewCategory = new CategoryDto(0, "");
+        IsCreating = true;
+    }
+
+    public void SaveNewCategory(CategoryDto category)
+    {
+        IsCreating = false;
+        NewCategory = null;
+        //_categoryService.AddCategory(category);
+        LoadCategories();
+    }
+
+    public void CancelCreate()
+    {
+        IsCreating = false;
+        NewCategory = null;
+    }
+
+    public void EditCategory(CategoryDto category)
+    {
+        //_categoryService.UpdateCategory(category);
+        LoadCategories();
+    }
+
+    public void DeleteCategory(long id)
+    {
+        //_categoryService.DeleteCategory(id);
+        LoadCategories();
     }
 }
