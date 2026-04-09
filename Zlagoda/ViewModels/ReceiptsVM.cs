b496a2a3dadec255af44d7c840f36e1bdea9ed05
SkyWarpX27X <mykhajloost@gmail.com>
2026-04-09 11:05:11 +0300

    Receipt CRUD frontend

--- Zlagoda/ViewModels/ReceiptsVM.cs
+++ Zlagoda/ViewModels/ReceiptsVM.cs
@@ -8,16 +8,24 @@ public class ReceiptsVM
     //TODO switch to real receipt service
     //private readonly IReceiptService _receiptService;
 
-    public IEnumerable<ReceiptDto> Receipts { get; set; }
+    public IEnumerable<ReceiptDTO> Receipts { get; private set; }
+    
+    public IEnumerable<CustomerDTO> Customers { get; private set; }
+    public IEnumerable<StoreProductDTO> StoreProducts { get; private set; }
 
     public string SelectedEmployee { get; set; } = "";
     public DateTime? FromDate { get; set; }
     public DateTime? ToDate { get; set; }
+    
+    public bool IsCreating { get; private set; }
+    public ReceiptCreateDTO? NewReceipt { get; private set; }
 
     public ReceiptsVM()
     {
         //_receiptService = receiptService;
-        Receipts = new List<ReceiptDto>();
+        Receipts = new List<ReceiptDTO>();
+        Customers = new List<CustomerDTO>();
+        StoreProducts = new List<StoreProductDTO>();
     }
 
     public IEnumerable<string> Employees => 
@@ -25,7 +33,7 @@ public class ReceiptsVM
 
     
     //TODO: Move filter to service layer after connecting it
-    public IEnumerable<ReceiptDto> FilteredReceipts
+    public IEnumerable<ReceiptDTO> FilteredReceipts
     {
         get
         { 
@@ -54,6 +62,10 @@ public class ReceiptsVM
     {
         // Receipts = _receiptService.GetReceipts();
         Receipts = FakeReceipts.GetReceipts();
+        
+        // TODO: Load from real services
+        Customers = FakeCustomers.GetCustomers();
+        StoreProducts = FakeStoreProducts.GetProducts();
     }
 
     public void ClearFilters()
@@ -63,13 +75,29 @@ public class ReceiptsVM
         ToDate = null;
     }
 
-    public void CreateReceipt()
+    public void ShowCreateNew()
     {
-        // TODO: Implement create
+        NewReceipt = new ReceiptCreateDTO( "", DateTime.Now, new List<SaleDTO>());
+        IsCreating = true;
+    }
+
+    public void SaveNewReceipt(ReceiptCreateDTO receipt)
+    {
+        IsCreating = false;
+        NewReceipt = null;
+        // TODO: save receipt via service
+        LoadReceipts();
+    }
+
+    public void CancelCreate()
+    {
+        IsCreating = false;
+        NewReceipt = null;
     }
 
     public void DeleteReceipt(string id)
     {
-        // TODO: Implement delete
+        // TODO: delete receipt via service
+        LoadReceipts();
     }
 }
