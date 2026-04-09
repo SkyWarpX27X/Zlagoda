aa1acc22231cf36b4de22a188f32c929d71c1882
SkyWarpX27X <mykhajloost@gmail.com>
2026-04-09 11:03:20 +0300

    Create DTO

---
+++ DTOModels/Receipt/ReceiptCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace DTOModels;
+
+public class ReceiptCreateDTO
+{
+    public string CustomerCardId { get; set; }
+    public DateTime PrintDate { get; set; }
+    public List<SaleDTO> Sales { get; set; }
+
+    public ReceiptCreateDTO(string customerCardId, DateTime printDate, List<SaleDTO> sales)
+    {
+        CustomerCardId = customerCardId;
+        PrintDate = printDate;
+        Sales = sales;
+    }
+}
