e279bd7ea54fb1df94279d4369dfb12744f86022
wwhasmile <uwuhasmile@proton.me>
2026-04-10 19:18:56 +0300

    Customer service implementation

--- Services/Customer/ICustomerService.cs
+++ Services/Customer/ICustomerService.cs
@@ -4,8 +4,8 @@ namespace Services.Customer;
 
 public interface ICustomerService
 {
-    IEnumerable<CustomerDTO> GetCustomers();
-    CustomerDTO GetCustomer(int id);
+    IEnumerable<CustomerDTO> GetCustomers(int percent = -1);
+    CustomerDTO? GetCustomer(string cardId);
     void AddCustomer(CustomerModifyDTO customer);
     void UpdateCustomer(CustomerModifyDTO customer);
     void DeleteCustomer(string cardId);
