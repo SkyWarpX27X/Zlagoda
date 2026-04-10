0d1c9fa100a3bc3f51bc77296fa8d021538f6f57
SkyWarpX27X <mykhajloost@gmail.com>
2026-04-10 14:18:31 +0300

    Add descripton for products in store

--- Zlagoda/Test/FakeStoreProducts.cs
+++ Zlagoda/Test/FakeStoreProducts.cs
@@ -10,8 +10,8 @@ static class FakeStoreProducts
     {
         _products = new List<StoreProductDTO>()
         {
-            new StoreProductDTO("12345678", "Газета Кишинівський Підмічатель", 30, 100, false, null),
-            new StoreProductDTO("3123132", "Повітря", 20, 100, true, 30)
+            new StoreProductDTO("12345678", "Газета Кишинівський Підмічатель", 30, "Головний спонсор ремонту дороги в Бєльцях",100, false, null),
+            new StoreProductDTO("3123132", "Повітря", 20, "Чисте повітря",100, true, 30)
         };
     }
     
