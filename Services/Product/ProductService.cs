ab0b74ac2cf1c81739b5241f1f444888d825d5e6
wwhasmile <uwuhasmile@proton.me>
2026-04-11 21:05:18 +0300

    Product Service refactoring

--- Services/Product/ProductService.cs
+++ Services/Product/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using DTOModels;
+﻿using DBModels;
+using DTOModels;
 using Repositories.Category;
 using Repositories.Product;
 
@@ -19,21 +20,45 @@ public class ProductService : IProductService
     {
         foreach (var product in _productRepository.GetProducts(categoryName: categoryName))
         {
-            var category = _categoryRepository.GetCategory(product.CategoryId);
-            if (category is null) throw new InvalidDataException($"Category {product.CategoryId} does not exist");
-            yield return new(
-                product.Id,
-                product.Name,
-                category.Name,
-                product.Characteristics,
-                product.Manufacturer);
+            yield return ProductDbToDto(product);
         }
     }
 
-    public ProductDTO? GetProduct(long id)
+    public ProductDTO GetProduct(long id)
     {
         var product = _productRepository.GetProduct(id);
-        if (product is null) return null;
+        if (product is null) throw new InvalidDataException($"Product {id} does not exist");
+        return ProductDbToDto(product);
+    }
+
+    public void AddProduct(ProductDTO product)
+    {
+        ValidateProduct(product, out CategoryDBModel? category);
+        _productRepository.AddProduct(new(
+            category!.Id,
+            product.Name,
+            product.Characteristics,
+            product.Manufacturer));
+    }
+
+    public void UpdateProduct(ProductDTO product)
+    {
+        ValidateProduct(product, out CategoryDBModel? category);
+        _productRepository.UpdateProduct(new(
+            product.Id,
+            category!.Id,
+            product.Name,
+            product.Characteristics,
+            product.Manufacturer));
+    }
+
+    public void DeleteProduct(long id)
+    {
+        _productRepository.DeleteProduct(id);
+    }
+
+    private ProductDTO ProductDbToDto(ProductDBModel product)
+    {
         var category = _categoryRepository.GetCategory(product.CategoryId);
         if (category is null) throw new InvalidDataException($"Category {product.CategoryId} does not exist");
         return new(
@@ -44,41 +69,13 @@ public class ProductService : IProductService
             product.Manufacturer);
     }
 
-    public void AddProduct(ProductDTO product)
+    private void ValidateProduct(ProductDTO product, out CategoryDBModel? category)
     {
         if (string.IsNullOrEmpty(product.Name)) throw new InvalidDataException("Name is required");
         if (string.IsNullOrEmpty(product.Category)) throw new InvalidDataException("Category is required");
-        var category = _categoryRepository.GetCategories(false).FirstOrDefault(
-            x => x.Name == product.Category);
+        category = _categoryRepository.GetCategory(product.Category);
         if (category is null) throw new InvalidDataException($"Category {product.Category} does not exist");
         if (string.IsNullOrEmpty(product.Characteristics)) throw new InvalidDataException("Characteristics are required");
         if (string.IsNullOrEmpty(product.Manufacturer)) throw new InvalidDataException("Manufacturer is required");
-        _productRepository.AddProduct(new(
-            category.Id,
-            product.Name,
-            product.Characteristics,
-            product.Manufacturer));
-    }
-
-    public void UpdateProduct(ProductDTO product)
-    {
-        if (string.IsNullOrEmpty(product.Name)) throw new InvalidDataException("Name is required");
-        if (string.IsNullOrEmpty(product.Category)) throw new InvalidDataException("Category is required");
-        var category = _categoryRepository.GetCategories(false).FirstOrDefault(
-            x => x.Name == product.Category);
-        if (category is null) throw new InvalidDataException($"Category {product.Category} does not exist");
-        if (string.IsNullOrEmpty(product.Characteristics)) throw new InvalidDataException("Characteristics are required");
-        if (string.IsNullOrEmpty(product.Manufacturer)) throw new InvalidDataException("Manufacturer is required");
-        _productRepository.UpdateProduct(new(
-            product.Id,
-            category.Id,
-            product.Name,
-            product.Characteristics,
-            product.Manufacturer));
-    }
-
-    public void DeleteProduct(long id)
-    {
-        _productRepository.DeleteProduct(id);
     }
 }
