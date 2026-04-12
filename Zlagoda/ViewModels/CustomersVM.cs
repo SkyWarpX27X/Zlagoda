05e21b626a3cc3e7c46750940455b753f1d90b11
SkyWarpX27X <mykhajloost@gmail.com>
2026-04-12 16:05:33 +0300

    Customers validation error handling

--- Zlagoda/ViewModels/CustomersVM.cs
+++ Zlagoda/ViewModels/CustomersVM.cs
@@ -7,55 +7,67 @@ public class CustomersVM
 {
     private readonly ICustomerService _customerService;
     
-    public IEnumerable<CustomerDTO> Customers;
-    public bool IsCreating;
-    public CustomerModifyDTO? NewCustomer;
-    public int? SearchPercent { get; set; }
-    
-    public IEnumerable<CustomerDTO> FilteredCustomers =>
+    public IEnumerable<CustomerDTO> Customers =>
         SearchPercent.HasValue
             ? _customerService.GetCustomers(SearchPercent.Value)
             : _customerService.GetCustomers();
+    public bool IsCreating;
+    public CustomerModifyDTO? NewCustomer;
+    public int? SearchPercent { get; set; }
+    public string? ErrorMessage { get; private set; }
     
     public CustomersVM(ICustomerService customerService)
     {
         _customerService = customerService;
-        Customers = new List<CustomerDTO>();
-    }
-
-    public void LoadCustomers()
-    {
-        Customers = _customerService.GetCustomers();
     }
     
     public void ShowCreateNew()
     {
         NewCustomer = new CustomerModifyDTO();
         IsCreating = true;
+        ErrorMessage = null;
     }
     
     public void CancelCreate()
     {
         IsCreating = false;
         NewCustomer = null;
+        ErrorMessage = null;
+    }
+    
+    public void ClearError()
+    {
+        ErrorMessage = null;
     }
     
     public void SaveNewCustomer(CustomerModifyDTO customer)
     {
-        _customerService.AddCustomer(customer);
-        IsCreating = false;
-        LoadCustomers();
+        try
+        {
+            _customerService.AddCustomer(customer);
+            IsCreating = false;
+            ErrorMessage = null;
+        }
+        catch (InvalidDataException e)
+        {
+            ErrorMessage = e.Message;
+        }
     }
 
     public void EditCustomer(CustomerModifyDTO customer)
     {
-        _customerService.UpdateCustomer(customer);
-        LoadCustomers();
+        try
+        {
+            _customerService.UpdateCustomer(customer);
+        }
+        catch (InvalidDataException e)
+        {
+            ErrorMessage = e.Message;
+        }
     }
 
     public void DeleteCustomer(string cardId)
     {
         _customerService.DeleteCustomer(cardId);
-        LoadCustomers();
     }
 }
