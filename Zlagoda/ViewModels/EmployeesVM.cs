97ea8119f6ed5fe4d3e8ec3e73395adbf5d2cc6f
SkyWarpX27X <mykhajloost@gmail.com>
2026-04-12 21:17:06 +0300

    Employees error handling

--- Zlagoda/ViewModels/EmployeesVM.cs
+++ Zlagoda/ViewModels/EmployeesVM.cs
@@ -6,49 +6,83 @@ namespace Zlagoda.ViewModels;
 
 public class EmployeesVM
 {
-    //TODO switch to real employee service
     private readonly IEmployeeService _employeeService;
 
-    public IEnumerable<EmployeeDTO> Employees { get; private set; }
+    public IEnumerable<EmployeeDTO> Employees => FilterEmployees();
     
     public bool IsCreating { get; private set; }
     public EmployeeModifyDTO? NewEmployee { get; private set; }
+    public string? ErrorMessage { get; private set; }
 
     public EmployeesVM(IEmployeeService employeeService)
     {
         _employeeService = employeeService;
-        Employees = new List<EmployeeDTO>();
     }
 
-    public void LoadEmployees()
+    public string SearchSurname { get; set; } = "";
+    public string RoleFilter { get; set; } = "All";
+    
+    public IEnumerable<EmployeeDTO> FilterEmployees()
     {
-        Employees = _employeeService.GetEmployees();
+        if (RoleFilter == "Cashier")
+        {
+            if (!string.IsNullOrWhiteSpace(SearchSurname))
+            {
+                return _employeeService.SearchEmployees(SearchSurname, true);
+            }
+            return _employeeService.GetEmployees(true);
+        }
+        if (!string.IsNullOrWhiteSpace(SearchSurname))
+        {
+            return  _employeeService.SearchEmployees(SearchSurname);
+        }
+        return _employeeService.GetEmployees(false);
     }
     
     public void ShowCreateNew()
     {
         NewEmployee = new EmployeeModifyDTO(null, "", "", null, "", "", "", 0, DateOnly.FromDateTime(DateTime.Now), DateOnly.FromDateTime(DateTime.Now), "", "", "", "");
         IsCreating = true;
+        ErrorMessage = null;
     }
 
     public void SaveNewEmployee(EmployeeModifyDTO employee)
     {
-        IsCreating = false;
-        NewEmployee = null;
-        _employeeService.AddEmployee(employee);
-        LoadEmployees();
+        try
+        {
+            _employeeService.AddEmployee(employee);
+            IsCreating = false;
+            NewEmployee = null;
+            ErrorMessage = null;
+        }
+        catch (InvalidDataException e)
+        {
+            ErrorMessage = e.Message;
+        }
     }
 
     public void CancelCreate()
     {
         IsCreating = false;
         NewEmployee = null;
+        ErrorMessage = null;
+    }
+    
+    public void ClearError()
+    {
+        ErrorMessage = null;
     }
 
     public void EditEmployee(EmployeeModifyDTO employee)
     {
-        _employeeService.UpdateEmployee(employee);
-        LoadEmployees();
+        try
+        {
+            _employeeService.UpdateEmployee(employee);
+        }
+        catch (InvalidDataException e)
+        {
+            ErrorMessage = e.Message;
+        }
     }
 
     public void DeleteEmployee(long id)
